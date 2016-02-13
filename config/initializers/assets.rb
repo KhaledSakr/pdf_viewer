2c06398d5fb3605a36518a495f99e701949e08a4
Osama Jandali <osamajandaly@gmail.com>
2016-02-13 02:29:20 +0200

    Doing login frontend
    - Make design for login
    - Create HTML & CSS
    - Implement this frontend code with the backend

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( default.css )
+Rails.application.config.assets.precompile += %w( style.css )
 Rails.application.config.assets.precompile += %w( default.js )
+Rails.application.config.assets.precompile += %w( script.js )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
