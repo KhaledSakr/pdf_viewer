1da730fa573437e1dff80afcc2dacde2c67fcd06
Osama Jandali <osamajandaly@gmail.com>
2016-02-19 14:13:26 +0200

    Edit uploads

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,3 +2,4 @@ class Upload < ActiveRecord::Base
   has_attached_file :document
 	validates_attachment :document, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
 end
+ 
