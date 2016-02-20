7c8121297209593b7884df1d1a5283e7530162ec
Osama Jandali <osamajandaly@gmail.com>
2016-02-20 22:30:35 +0200

    Adding the separation code
    - Separate Pdfs code

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,14 +1,35 @@
 class SlidesController < ApplicationController
   def new
 	end
-		
+	
+
 	
   def index
   	@uploads = Upload.all
   end
   def show
+
     @upload = Upload.find(params[:id])
+    
+    #Start from here ! 
+    @test = @upload.document.url
+    @test = @test.partition('?').first
+    
+    #To convert pdf to png, pdf is an array has all the pdf pages as images
+    pdf = Magick::ImageList.new("public" + @test)
+    
+    count = pdf.count #count = pdf's number of pages
+        
+    @file_name = @upload.document_file_name # @file_name = The pdf file name
+    @file_name = @file_name.partition('.').first 
+    
+    #Loop to convert every page for an image
+    for i in 0...count
+        #before [@file_name + "#{i}.png"] you can edit where to save the images
+      png = pdf[i].write(@file_name + "#{i}.png") 
+    end
   end
+
   def destroy
     @upload = Upload.find(params[:id])
     @upload.destroy
